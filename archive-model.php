93709e098dbaeb39fdf37b5af09da7cf47aaf867
LogicDuke <fierensstefan08@gmail.com>
2026-01-17 17:43:16 +0100

    Update archive-model.php

--- archive-model.php
+++ archive-model.php
@@ -33,7 +33,7 @@ if ($models_page) {
   <div class="tmw-layout container">
     <section class="tmw-content" data-mobile-guard="true">
       <header class="entry-header tmw-models-archive-header">
-        <h1 class="widget-title"><span class="tmw-star">★</span> Models</h1>
+        <h1 class="widget-title"><span class="tmw-star">★</span> Top Models</h1>
       </header>
       
       <?php if (!empty($seo_text)) : ?>
