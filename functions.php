c3d326dbcbf605ae54b4327fd98bde197483a663
LogicDuke <fierensstefan08@gmail.com>
2026-01-18 11:59:56 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -186,3 +186,19 @@ add_filter('site_transient_update_themes', function($value) {
 
     return $value;
 });
+
+/**
+ * Enqueue TMW Beautiful Accordion styling for video/model pages
+ */
+add_action('wp_enqueue_scripts', 'tmw_enqueue_beautiful_accordion_css', 20);
+
+function tmw_enqueue_beautiful_accordion_css() {
+    if (is_singular('post') || is_singular('model') || is_singular('video')) {
+        wp_enqueue_style(
+            'tmw-beautiful-accordion',
+            get_stylesheet_directory_uri() . '/css/tmw-beautiful-accordion.css',
+            array(),
+            '1.0.0'
+        );
+    }
+}
